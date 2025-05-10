bd067b10051a374515252104e7b47e12df8903f2
ThaonxTASC <96766829+phomit03@users.noreply.github.com>
2025-05-10 10:47:14 +0700

    update featured-box

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -21,4 +21,18 @@ $(document).ready(function(){
             }
         }
     });
+
+    $('.featured-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: { items: 1 },
+            576: { items: 2 },
+            768: { items: 3 },
+            992: { items: 4 }
+        }
+    });
+
 });
